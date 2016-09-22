0478e53548562cd9c8db3faa1e9b23386423cef9
Joao Teodoro <joaotdn@gmail.com>
2016-09-22 16:38:02 -0300

    atualizado

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,19 @@
 $(document).foundation();
+
+(function($) {
+
+    /**
+     * Menu mobile
+     */
+    //Botão abrir/fechar menu
+    $('*[data-show-menu]').click(function (e) {
+        e.preventDefault();
+        $('#wrapper').toggleClass('move');
+        $('.close-mo-menu, #mo-menu').toggleClass('active');
+    });
+
+    //Clonar menu
+    $('ul','#main-menu').clone().appendTo('#mo-menu').addClass('vertical');
+
+
+})(jQuery);
